cea1053020485141f505e737e014f17d7c05de7f
OctaviaGithub <enzolahaye@yahoo.com>
2023-11-23 22:59:13 +0000

    Commit du 23/11. Fin de journée : 22h58 Informations pour prochains tests : faire en sorte de faire fonctionner l'input pour une redirection ? Test pour faire d'autres pages avec vue.js ?

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # dek-s-auto
+
+Notes du 23/11 : Début d'éxécution de l'input. J'arrive à prendre la valeur que je rentre dedans. Maintenant, à voir pour une redirection selon la demande.
--- my_express_app/app.js
+++ my_express_app/app.js
@@ -1,8 +1,46 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
+app.use(express.json());
+
+
+// J'accepte toutes requêtes venant de mon localhost:8080
+app.use(cors({
+  origin: 'http://localhost:8080' 
+}));
+
+app.get('/api/message', (req, res) => {
+  res.json({ message: 'https://images.ctfassets.net/uaddx06iwzdz/1sfLB4IrdwYgUAZBoT3phQ/5cde5764f2813e686f5fc54980099161/bmw-x4-l-02.jpg' });
+});
+const items = [
+  { id: 1, name: 'Peugeot Focus' },
+  { id: 2, name: 'Porshe 911' },
+];
+app.get('/api/items', (req, res) => {
+  res.json(items);
+});
+
+
+
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
+});
+
+app.get('/maroute/plusderoute', (req, res) => {
+  const value = req.query.value;
+  res.send(`Received value: ${value}`);
+});
+
+
+
+app.post('/api/add-item', (req, res) => {
+  // Accéder aux données envoyées dans la requête POST via req.body
+  const newItem = req.body;
+  console.log(newItem); // Traiter l'item reçu (comme l'ajouter dans une base de données)
+
+  // Envoyer une réponse au client
+  res.status(201).send('Item ajouté avec succès');
 });
 
 const PORT = process.env.PORT || 3000;
